4952ead61a668f61c08140841c0ff09a62c21baa
Damyan Peykov <dppeykov@gmail.com>
2019-10-15 11:15:33 +0200

    Unittest for the code

---
+++ test.py
@@ -0,0 +1,29 @@
+# Never runs in production - just for development to check if all works fine
+
+import unittest
+import string_reversal
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        print('TESTING CASE')
+
+    def test_rev_loop(self):
+        '''Testing the reverse loop with hello'''
+        s = "hello"
+        reversed_s = string_reversal.rev_loop(s)
+        self.assertEqual(reversed_s,"olleh")
+
+    def test_rev_loop2(self):
+        s = "apple"
+        reversed_s = string_reversal.rev_loop(s)
+        self.assertEqual(reversed_s,"elppa")
+
+    def tearDown(self):  # for more complicated tests
+        print("CLEANING UP")
+
+# to make sure that this is the main file that can be ran - use the __name__ - in that way we are sure that it will run only the test
+# in this way we can run multiple test files in the same module at once - python -m unittest (-v = verbose)
+if __name__ == "__main__":
+    # this is just the main function of the unittest module - just run all the tests in the file
+    unittest.main() 
